0eaeb3e621a10c166c16225b877c1d647d90096a
DESKTOP-G2RUKHA\F309 <D1074181051@gm.lhu.edu.tw>
2020-11-19 15:38:36 +0800

    team08

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<hr>
+<a style="color: blueviolet" href="{{ route('antiques.index')}}"class="ml-1 underline">
+    所有古物
+</a><br>
+
+<a style="color: darkmagenta" href="{{route('dynastys.index')}}"class="ml-1 underline">
+    所有朝代
+</a>
