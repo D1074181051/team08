061f1d6e9f55dc9ecd763eda7be9c43a5750c1ae
DESKTOP-G2RUKHA\F309 <D1074181051@gm.lhu.edu.tw>
2021-01-07 14:07:25 +0800

    team08

--- app/Http/Requests/CreateDynastyRequest.php
+++ app/Http/Requests/CreateDynastyRequest.php
@@ -25,7 +25,7 @@ class CreateDynastyRequest extends FormRequest
     {
         return [
             't_name' => 'required|string',
-            's_time' => 'required|numeric|lte:e_time',
+            's_time' => 'required|numeric|dateearlier:e_time',
             'e_time' => 'required|numeric',
             'capital' => 'required|string'
         ];
@@ -39,7 +39,7 @@ class CreateDynastyRequest extends FormRequest
             "s_time.required" => "經歷時間(起)為必填",
             "s_time.numeric" => "經歷時間(起)必需為數字",
             //"s_time.min" => "經歷時間(起)必需大於0",
-            "s_time.lte" => "經歷時間(迄)必須大於經歷時間(起)",
+            "s_time.dateearlier" => "經歷時間(迄)必須大於經歷時間(起)",
             "e_time.required" => "經歷時間(迄)為必填",
             "e_time.numeric" => "經歷時間(迄)必需為數字",
             //"e_time.min" => "經歷時間(迄)必需大於0",
